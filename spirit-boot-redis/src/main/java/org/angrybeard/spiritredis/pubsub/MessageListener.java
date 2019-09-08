16ff24af7dcc43a6ee5ee0a3638d5d2f90cb9743
angry_beard <better_river@163.com>
2019-09-08 22:46:10 +0800

    update

--- spirit-boot-redis/src/main/java/org/angrybeard/spiritredis/pubsub/MessageListener.java
+++ spirit-boot-redis/src/main/java/org/angrybeard/spiritredis/pubsub/MessageListener.java
@@ -2,7 +2,11 @@ package org.angrybeard.spiritredis.pubsub;
 
 import lombok.extern.slf4j.Slf4j;
 import org.angrybeard.spiritredis.dto.Message;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * Created by angry_beary on 2019/9/8.
@@ -11,7 +15,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageListener {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     public void receiveMessage(Message message, String pattern) {
-        log.info("topic ={} received={} ", pattern, message);
+        if (isRan(message.getId(), "ok")) {
+            log.info("topic ={} received={} ", pattern, message);
+        }
+    }
+
+    private boolean isRan(String key, Object value) {
+        return stringRedisTemplate.execute((RedisConnection conn) -> {
+            try {
+                return conn.setNX(stringRedisTemplate.getStringSerializer().serialize(key),
+                        stringRedisTemplate.getStringSerializer().serialize(value.toString()));
+            } finally {
+                conn.close();
+            }
+        });
+
     }
 }
